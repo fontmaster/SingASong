160cfb979d1995846c89dc0cf04125385da6adc8
조상정 <sangjeong.jo@samsung.com>
2022-10-21 04:41:26 +0900

    Refactoring #2
    - change animals to args[]

--- src/test/java/SongTest.java
+++ src/test/java/SongTest.java
@@ -11,7 +11,10 @@ class SongTest {
 
     @Test
     void approvalTest() {
-        Approvals.verify(Song.makeSong());
+//        Approvals.verify(Song.makeSong());
+        // old lady cow -> dog -> cat -> bird -> spider -> fly
 
+        String[] animals = {"fly","spider","bird","cat","dog","cow"};
+        Approvals.verify(Song.makeSong(animals));
     }
 }
